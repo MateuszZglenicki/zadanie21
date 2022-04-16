bd9183a21878fe0a41453d59ebe85ce7e8aa1cf5
MateuszZglenicki <matizglen25@wp.pl>
2022-04-16 16:41:52 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/javastart/zadanie21/Zadanie21Application.java
@@ -0,0 +1,13 @@
+package pl.javastart.zadanie21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie21Application.class, args);
+    }
+
+}
